5d5f4dcecd8d90f3bc2a6878ab4e4d66dab90110
BagusFatkhur <bagusfatkhur03@gmail.com>
2024-07-17 10:42:57 +0700

    memperbaiki slide gambar

--- halaman.php
+++ halaman.php
@@ -1,86 +1,80 @@
-
-<div class="akses">GG
-<!-- corosuel -->
-<div class="col-md-12">
-<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-  </div>
-  <meta charset="UTF-8">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <!-- Tambahkan link ke Google Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Pacifico:wght@400;700&display=swap">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         /* Tambahkan aturan CSS untuk font baru */
         .custom-font {
             font-family: 'Pacifico', serif;
         }
     </style>
-   <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="img/slide/slide1.jpg" class="d-block w-100">
-            <div class="carousel-caption d-none d-md-block position-absolute top-50 start-50 translate-middle custom-font">
-                <h1>Selamat Datang</h1>
-                <h4>Di Websaite Resmi Masjid <b>Al-Furqon</b></h4>
-            </div>
-        </div>
-    </div>
-    <div class="carousel-item">
-      <img src="img/slide/slide2.jpg" class="d-block w-100" >
-      <div class="col-9 carousel-caption d-none d-md-block position-absolute top-50 start-50 translate-middle">
-        <h1>Sekilas Interior Masjid</h1>
-        <h4>Masjid merupakan tempat beribadah masayrakat muslim, untuk menambah kenyaman dalam beribadah tidak jarang pengelola masjid membuat desain yang menenangkan,minimalis,segar,sederhana namun sangat efektif untuk berbagai keperluan dan kegiatan.</h4>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-</div>
-<!-- corosuel -->
-
-<!-- sejarah -->
-<section class="container mt-5">
-  <div class="row">
-  <div class="col">
-      <img src="img/hero.jpg" class="rounded" width="600" height="400">
-    </div>
-  <div class="col">
-      <h3 class="mb-3">Sejarah</h2>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam quis eius cum fugit unde aperiam, ad perspiciatis nesciunt ipsa ut rem quasi iure amet aliquam ipsum natus necessitatibus quibusdam autem non adipisci voluptatum quaerat, fugiat sapiente soluta. Delectus quae nostrum ipsum dolore dolorem architecto, doloremque voluptas optio minima temporibus laudantium!</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam quis eius cum fugit unde aperiam, ad perspiciatis nesciunt ipsa ut rem quasi iure amet aliquam ipsum natus necessitatibus quibusdam autem non adipisci voluptatum quaerat, fugiat sapiente soluta. Delectus quae nostrum ipsum dolore dolorem architecto, doloremque voluptas optio minima temporibus laudantium!</p>
-      
-    </div>
-  </div>
-</section>
-<!-- sejarah -->
-
-<!-- sarana -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sarana | Prasarana</title>
-    <link rel="stylesheet" href="boostrap/css/style.css">
 </head>
 <body>
-    <!-- konten -->
+<div class="akses">
+    <!-- carousel -->
+    <div class="col-md-12">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="3000">
+            <div class="carousel-indicators">
+                <button type="button" data-target="#carouselExampleIndicators" data-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-target="#carouselExampleIndicators" data-slide-to="1" aria-label="Slide 2"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="img/slide/slide1.jpg" class="d-block w-100">
+                    <div class="carousel-caption d-none d-md-block position-absolute top-50 start-50 translate-middle custom-font">
+                        <h1>Selamat Datang</h1>
+                        <h4>Di Website Resmi Masjid <b>Al-Fakhtur</b></h4>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="img/slide/slide2.jpg" class="d-block w-100">
+                    <div class="col-9 carousel-caption d-none d-md-block position-absolute top-50 start-50 translate-middle">
+                        <h1>Sekilas Interior Masjid</h1>
+                        <h4>Masjid merupakan tempat beribadah masayrakat muslim, untuk menambah kenyaman dalam beribadah tidak jarang pengelola masjid membuat desain yang menenangkan, minimalis, segar, sederhana namun sangat efektif untuk berbagai keperluan dan kegiatan.</h4>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-target="#carouselExampleIndicators" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+    <!-- carousel -->
+
+    <!-- sejarah -->
+    <section class="container mt-5">
+        <div class="row">
+            <div class="col">
+                <img src="img/hero.jpg" class="rounded" width="600" height="400">
+            </div>
+            <div class="col">
+                <h3 class="mb-3">Sejarah</h3>
+                <p>Masjid (serapan dari bahasa Arab: مَسْجِد, translit. masjid, pelafalan [mǝsdʒid]; secara harfiah "tempat sujud"), merupakan tempat salat bagi umat Islam. Masjid biasanya tertutup bangunan, tetapi bisa menjadi tempat salat (sujud) dilakukan, termasuk halaman luar.</p>
+                <p>Awalnya masjid adalah tempat salat sederhana bagi umat Islam, dan mungkin merupakan ruang terbuka daripada bangunan. Pada tahap pertama arsitektur Islam, 650-750, masjid terdiri dari ruang terbuka dan tertutup yang dikelilingi oleh dinding, seringkali dengan menara tempat azan dikeluarkan. Bangunan masjid biasanya berisi mihrab dipasang di dinding yang menunjukkan arah Kiblat ke Makkah, dan fasilitas wudhu. Mimbar, tempat di mana khutbah salat Jumat disampaikan, dulunya adalah karakteristik masjid pusat kota, tetapi sejak itu menjadi umum di masjid-masjid kecil. Masjid biasanya memiliki ruang terpisah untuk pria dan wanita. Pola dasar organisasi ini mengambil bentuk yang berbeda tergantung pada wilayah, periode, dan mazhab.</p>
+            </div>
+        </div>
+    </section>
+    <!-- sejarah -->
+
+    <!-- sarana -->
     <section id="pengumuman">
         <div class="container mt-5">
             <div class="row py-4">
                 <div class="col-md-6 mx-auto">
                     <h4 class="fw-bold text-center">Sarana | Prasarana</h4>
                     <!-- carousel -->
-                    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel" data-interval="3000">
                         <div class="carousel-indicators">
                             <?php 
                             $i = 0;
@@ -88,7 +82,7 @@
                             $query = $conn->query("SELECT * FROM sarana");
                             while($data = mysqli_fetch_assoc($query)) {
                                 $active = $i == 0 ? 'active' : '';
-                                echo '<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="' . $i . '" class="' . $active . '"></button>';
+                                echo '<button type="button" data-target="#carouselExampleCaptions" data-slide-to="' . $i . '" class="' . $active . '"></button>';
                                 $i++;
                             }
                             ?>
@@ -107,11 +101,11 @@
                             }
                             ?>
                         </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                        <button class="carousel-control-prev" type="button" data-target="#carouselExampleCaptions" data-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Previous</span>
                         </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                        <button class="carousel-control-next" type="button" data-target="#carouselExampleCaptions" data-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Next</span>
                         </button>
@@ -120,50 +114,35 @@
             </div>
         </div>
     </section>
-    <!-- konten -->
+    <!-- sarana -->
+
+    <!-- pengurus -->
+    <section class="container mt-5">
+        <div class="row">
+            <div class="col">
+                <div class="mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-8">
+                            <img src="img/sosial/pengurus.jpg" class="img-fluid rounded-start" width="700">
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card-body">
+                                <h4 class="card-title">Pengurus</h4>
+                                <p class="card-text">Gambar bersama dengan pengurus DKM masjid Al-Furqon.</p>
+                                <a href="?page=organisasi" class="btn btn-primary text-decoration">Struktur Pengurus</a>
+                                <p class="card-text"><small class="text-muted">12/Aug/2022</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- pengurus -->
+
+    <!-- Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
-
-    
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-      <!-- casosul -->
-     
-</section>
-<!-- sarana -->
-
-
-
-<!-- pengurus -->
-<section class="container mt-5">
-  <div class="row">
-    <div class="col">
-    <div class=" mb-3">
-  <div class="row g-0">
-    <div class="col-md-8">
-      <img src="img/sosial/pengurus.jpg" class="img-fluid rounded-start" width="700">
-    </div>
-    <div class="col-md-4">
-      <div class="card-body">
-        <h4 class="card-title">Pengurus</h4>
-        <p class="card-text">Gambar bersama dengan pengurus DKM masjid Al-Furqon.</p>
-        <a href="?page=organisasi" class="btn btn-primary text-decoration">Struktur Pengurus</a>
-        <p class="card-text"><small class="text-muted">12/Aug/2022</small></p>
-      </div>
-    </div>
-  </div>
-</div>
-    </div>
-  </div>
-</section>
